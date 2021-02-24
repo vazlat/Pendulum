167db41157dadb6349784a41b004df1a904e0ddb
Vladimir Zlatoustov <vazlat1998@mail.ru>
2021-02-24 22:15:07 +0300

    connect form1 and graphic class

--- Form1.cs
+++ Form1.cs
@@ -12,54 +12,83 @@ namespace Pendulum
 {
     public partial class Form1 : Form
     {
+        Graph m_graphFlip;
+        Graph m_graphStabilization;
+
         public Form1()
         {
             InitializeComponent();
+
+            m_graphFlip = new Graph(panelFlip.CreateGraphics());
+            m_graphFlip.GraphicsCenter = new Point(panelFlip.Width / 2, panelFlip.Height / 2);
+            m_graphFlip.ZeroPointPosition = ZeroPointPosition.left;
+            m_graphFlip.StepXSize = 60;
+            m_graphFlip.StepY = 10;
+            m_graphFlip.StepYSize = 10;
+
+            m_graphStabilization = new Graph(panelStabilization.CreateGraphics());
+            m_graphStabilization.GraphicsCenter = new Point(panelStabilization.Width / 2, panelStabilization.Height / 2);
+            m_graphStabilization.ZeroPointPosition = ZeroPointPosition.left;
+            m_graphStabilization.StepXSize = 30;
+            m_graphStabilization.StepY = 0.5f;
+            m_graphStabilization.StepYSize = 20;
+
+            comboBoxFlipZerPoint.SelectedIndex = 1;
+            comboBoxStabilizationZerPoint.SelectedIndex = 1;
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void checkBoxFlipPoints_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxFlipSplain_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxFlipLines_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxStabilizationPoints_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxStabilizationSplain_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBoxStabilizationLines_CheckedChanged(object sender, EventArgs e)
-        {
-
+            m_graphFlip.Draw();
+            m_graphStabilization.Draw();
         }
 
         private void comboBoxFlipZerPoint_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            m_graphFlip.ZeroPointPosition = (ZeroPointPosition)comboBoxFlipZerPoint.SelectedIndex;
+            m_graphFlip.Draw();
         }
 
         private void comboBoxStabilizationZerPoint_SelectedIndexChanged(object sender, EventArgs e)
         {
+            m_graphStabilization.ZeroPointPosition = (ZeroPointPosition)comboBoxStabilizationZerPoint.SelectedIndex;
+            m_graphStabilization.Draw();
+        }
 
+        private void checkBoxFlipPoints_CheckedChanged(object sender, EventArgs e)
+        {
+            m_graphFlip.IsPoints = checkBoxFlipPoints.Checked;
+            m_graphFlip.Draw();
+        }
+
+        private void checkBoxFlipSplain_CheckedChanged(object sender, EventArgs e)
+        {
+            m_graphFlip.IsConnectingSpline = checkBoxFlipSplain.Checked;
+            m_graphFlip.Draw();
+        }
+
+        private void checkBoxFlipLines_CheckedChanged(object sender, EventArgs e)
+        {
+            m_graphFlip.IsConnectingLines = checkBoxFlipLines.Checked;
+            m_graphFlip.Draw();
+        }
+
+        private void checkBoxStabilizationPoints_CheckedChanged(object sender, EventArgs e)
+        {
+            m_graphStabilization.IsPoints = checkBoxStabilizationPoints.Checked;
+            m_graphStabilization.Draw();
+        }
+
+        private void checkBoxStabilizationSplain_CheckedChanged(object sender, EventArgs e)
+        {
+            m_graphStabilization.IsConnectingSpline = checkBoxStabilizationSplain.Checked;
+            m_graphStabilization.Draw();
+        }
+
+        private void checkBoxStabilizationLines_CheckedChanged(object sender, EventArgs e)
+        {
+            m_graphStabilization.IsConnectingLines = checkBoxStabilizationLines.Checked;
+            m_graphStabilization.Draw();
         }
     }
 }
